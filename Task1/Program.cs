c06765c376ee2cb87abef96ea9a82cbd34980261
Tasa <greatwillow@mail.ru>
2022-07-14 12:34:27 +0300

    first task

---
+++ Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] FillArray(int count)
+{
+    int[] array = new int[count];
+    Random rnd = new Random();
+
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int item in arr)
+    {
+        Console.Write($"{item} ");
+    }
+}
+
+int CountEvenNumber (int[] arr)
+{
+    int count = 0;
+     foreach (int item in arr)
+    {
+        if (item % 2 == 0)
+            count++;
+    }
+    return count;
+}
+Console.WriteLine("Введите количество элементов массива");
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+int[] array = FillArray(number);
+PrintArray(array);
+Console.WriteLine();
+int result = CountEvenNumber (array);
+Console.WriteLine(result);
